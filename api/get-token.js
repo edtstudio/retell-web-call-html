fd1630b7d7d34cb240217f1baf4f55daa2870043
edtstudio <edward@edtstudio.co.nz>
2025-08-16 23:16:11 +1200

    Update get-token.js

--- api/get-token.js
+++ api/get-token.js
@@ -2,11 +2,11 @@ import Retell from 'retell-sdk';
 
 export default async function handler(req, res) {
   const client = new Retell({
-    apiKey: 'key_665003e454b2acfed755143fdf51' // 🔁 Replace with your real API key
+    apiKey: key_665003e454b2acfed755143fdf51 // 🔁 Replace with your real API key
   });
 
   const response = await client.call.createWebCall({
-    agent_id: 'agent_18b445a7e16eacfc6181d47388' // 🔁 Replace with your real agent ID
+    agent_id: agent_18b445a7e16eacfc6181d47388 // 🔁 Replace with your real agent ID
   });
 
   res.status(200).json({
